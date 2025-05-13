7542b579c5368dc33f08242732fe82ca932d630c
andrii0lomakin <andrii0lomakin@gmail.com>
2025-05-13 08:57:15 +0200

    LocalPaginatedCollectionV2TestIT was fixed.

--- core/src/test/java/com/jetbrains/youtrack/db/internal/core/storage/StorageEncryptionTestIT.java
+++ core/src/test/java/com/jetbrains/youtrack/db/internal/core/storage/StorageEncryptionTestIT.java
@@ -2,19 +2,15 @@ package com.jetbrains.youtrack.db.internal.core.storage;
 
 import static org.junit.Assert.assertTrue;
 
-import com.jetbrains.youtrack.db.api.YouTrackDB;
+import com.jetbrains.youtrack.db.api.DatabaseType;
+import com.jetbrains.youtrack.db.api.YourTracks;
 import com.jetbrains.youtrack.db.api.config.GlobalConfiguration;
 import com.jetbrains.youtrack.db.api.config.YouTrackDBConfig;
 import com.jetbrains.youtrack.db.api.record.RID;
 import com.jetbrains.youtrack.db.api.schema.PropertyType;
-import com.jetbrains.youtrack.db.api.schema.Schema;
 import com.jetbrains.youtrack.db.api.schema.SchemaClass;
-import com.jetbrains.youtrack.db.internal.DbTestBase;
 import com.jetbrains.youtrack.db.internal.common.io.FileUtils;
 import com.jetbrains.youtrack.db.internal.core.db.DatabaseSessionInternal;
-import com.jetbrains.youtrack.db.internal.core.db.YouTrackDBConfigImpl;
-import com.jetbrains.youtrack.db.internal.core.db.YouTrackDBImpl;
-import com.jetbrains.youtrack.db.internal.core.record.impl.EntityImpl;
 import java.io.File;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -32,13 +28,13 @@ public class StorageEncryptionTestIT {
             .addGlobalConfigurationParameter(GlobalConfiguration.STORAGE_ENCRYPTION_KEY,
                 "T1JJRU5UREJfSVNfQ09PTA==")
             .build();
-    try (final YouTrackDB youTrackDB =
-        new YouTrackDBImpl(DbTestBase.embeddedDBUrl(getClass()), youTrackDBConfig)) {
-      youTrackDB.execute(
-          "create database encryption disk users ( admin identified by 'admin' role admin)");
-      try (var session = (DatabaseSessionInternal) youTrackDB.open("encryption", "admin",
+    try (final var youTrackDB =
+        YourTracks.embedded(dbDirectoryFile.getAbsolutePath(), youTrackDBConfig)) {
+      youTrackDB.createIfNotExists(StorageEncryptionTestIT.class.getSimpleName(), DatabaseType.DISK,
+          "admin", "admin", "admin");
+      try (var session = youTrackDB.open(StorageEncryptionTestIT.class.getSimpleName(), "admin",
           "admin")) {
-        final Schema schema = session.getMetadata().getSchema();
+        final var schema = session.getSchema();
         final var cls = schema.createClass("EncryptedData");
         cls.createProperty("id", PropertyType.INTEGER);
         cls.createProperty("value", PropertyType.STRING);
@@ -46,8 +42,9 @@ public class StorageEncryptionTestIT {
         cls.createIndex("EncryptedTree", SchemaClass.INDEX_TYPE.UNIQUE, "id");
         cls.createIndex("EncryptedHash", SchemaClass.INDEX_TYPE.UNIQUE, "id");
 
+        var tx = session.begin();
         for (var i = 0; i < 10_000; i++) {
-          final var document = ((EntityImpl) session.newEntity(cls));
+          final var document = tx.newEntity(cls);
           document.setProperty("id", i);
           document.setProperty(
               "value",
@@ -57,25 +54,29 @@ public class StorageEncryptionTestIT {
                   + " ");
 
         }
+        tx.commit();
 
+        tx = session.begin();
         final Random random = ThreadLocalRandom.current();
         for (var i = 0; i < 1_000; i++) {
           try (var resultSet =
-              session.query("select from EncryptedData where id = ?", random.nextInt(10_000_000))) {
+              tx.query("select from EncryptedData where id = ?", random.nextInt(10_000_000))) {
             if (resultSet.hasNext()) {
               final var result = resultSet.next();
               result.asEntity().delete();
             }
           }
         }
+        tx.commit();
       }
     }
 
-    try (final YouTrackDB youTrackDB =
-        new YouTrackDBImpl(
-            DbTestBase.embeddedDBUrl(getClass()), YouTrackDBConfig.defaultConfig())) {
+    try (var youTrackDB =
+        YourTracks.embedded(
+            dbDirectoryFile.getAbsolutePath(), YouTrackDBConfig.defaultConfig())) {
       try {
-        try (final var session = youTrackDB.open("encryption", "admin", "admin")) {
+        try (final var session = youTrackDB.open(StorageEncryptionTestIT.class.getSimpleName(),
+            "admin", "admin")) {
           Assert.fail();
         }
       } catch (Exception e) {
@@ -89,8 +90,8 @@ public class StorageEncryptionTestIT {
                 GlobalConfiguration.STORAGE_ENCRYPTION_KEY,
                 "DD0ViGecppQOx4ijWL4XGBwun9NAfbqFaDnVpn9+lj8=")
             .build();
-    try (final YouTrackDB youTrackDB =
-        new YouTrackDBImpl(DbTestBase.embeddedDBUrl(getClass()), wrongKeyOneYouTrackDBConfig)) {
+    try (final var youTrackDB =
+        YourTracks.embedded(dbDirectoryFile.getAbsolutePath(), wrongKeyOneYouTrackDBConfig)) {
       try {
         try (final var session = youTrackDB.open("encryption", "admin", "admin")) {
           Assert.fail();
@@ -106,8 +107,8 @@ public class StorageEncryptionTestIT {
                 GlobalConfiguration.STORAGE_ENCRYPTION_KEY,
                 "DD0ViGecppQOx4ijWL4XGBwun9NAfbqFaDnVpn9+lj8")
             .build();
-    try (final YouTrackDB youTrackDB =
-        new YouTrackDBImpl(DbTestBase.embeddedDBUrl(getClass()), wrongKeyTwoYouTrackDBConfig)) {
+    try (final var youTrackDB =
+        YourTracks.embedded(dbDirectoryFile.getAbsolutePath(), wrongKeyTwoYouTrackDBConfig)) {
       try {
         try (final var session = youTrackDB.open("encryption", "admin", "admin")) {
           Assert.fail();
@@ -117,40 +118,43 @@ public class StorageEncryptionTestIT {
       }
     }
 
-    try (final YouTrackDB youTrackDB =
-        new YouTrackDBImpl(DbTestBase.embeddedDBUrl(getClass()), youTrackDBConfig)) {
+    try (final var youTrackDB =
+        YourTracks.embedded(dbDirectoryFile.getAbsolutePath(), youTrackDBConfig)) {
       try (final var session =
-          (DatabaseSessionInternal) youTrackDB.open("encryption", "admin", "admin")) {
+          (DatabaseSessionInternal) youTrackDB.open(StorageEncryptionTestIT.class.getSimpleName(),
+              "admin", "admin")) {
         final var indexManager = session.getSharedContext().getIndexManager();
         final var treeIndex = indexManager.getIndex(session, "EncryptedTree");
         final var hashIndex = indexManager.getIndex(session, "EncryptedHash");
 
-        var entityIterator = session.browseClass("EncryptedData");
-        while (entityIterator.hasNext()) {
-          final var entity = entityIterator.next();
-          final int id = entity.getProperty("id");
-          final RID treeRid;
-          try (var rids = treeIndex.getRids(session, id)) {
-            treeRid = rids.findFirst().orElse(null);
-          }
-          final RID hashRid;
-          try (var rids = hashIndex.getRids(session, id)) {
-            hashRid = rids.findFirst().orElse(null);
+        session.executeInTx(tx -> {
+          var entityIterator = session.browseClass("EncryptedData");
+          while (entityIterator.hasNext()) {
+            final var entity = entityIterator.next();
+            final int id = entity.getProperty("id");
+            final RID treeRid;
+            try (var rids = treeIndex.getRids(session, id)) {
+              treeRid = rids.findFirst().orElse(null);
+            }
+            final RID hashRid;
+            try (var rids = hashIndex.getRids(session, id)) {
+              hashRid = rids.findFirst().orElse(null);
+            }
+
+            Assert.assertEquals(entity.getIdentity(), treeRid);
+            Assert.assertEquals(entity.getIdentity(), hashRid);
           }
 
-          Assert.assertEquals(entity.getIdentity(), treeRid);
-          Assert.assertEquals(entity.getIdentity(), hashRid);
-        }
-
-        Assert.assertEquals(session.countClass("EncryptedData"),
-            treeIndex.size(session));
-        Assert.assertEquals(session.countClass("EncryptedData"),
-            hashIndex.size(session));
+          Assert.assertEquals(session.countClass("EncryptedData"),
+              treeIndex.size(session));
+          Assert.assertEquals(session.countClass("EncryptedData"),
+              hashIndex.size(session));
+        });
       }
     }
   }
 
-  private File cleanAndGetDirectory() {
+  private static File cleanAndGetDirectory() {
     final var dbDirectory =
         "./target/databases" + File.separator + StorageEncryptionTestIT.class.getSimpleName();
     final var dbDirectoryFile = new File(dbDirectory);
@@ -162,33 +166,31 @@ public class StorageEncryptionTestIT {
   public void testEncryptionSingleDatabase() {
     final var dbDirectoryFile = cleanAndGetDirectory();
 
-    try (final YouTrackDB youTrackDB =
-        new YouTrackDBImpl(
-            DbTestBase.embeddedDBUrl(getClass()), YouTrackDBConfig.defaultConfig())) {
-      final var youTrackDBConfig =
-          YouTrackDBConfig.builder()
-              .addGlobalConfigurationParameter(GlobalConfiguration.STORAGE_ENCRYPTION_KEY,
-                  "T1JJRU5UREJfSVNfQ09PTA==")
-              .build();
+    final var youTrackDBConfig =
+        YouTrackDBConfig.builder()
+            .addGlobalConfigurationParameter(GlobalConfiguration.STORAGE_ENCRYPTION_KEY,
+                "T1JJRU5UREJfSVNfQ09PTA==")
+            .build();
 
-      youTrackDB.execute(
-          "create database encryption disk users ( admin identified by 'admin' role admin)");
+    try (final var youTrackDB =
+        YourTracks.embedded(dbDirectoryFile.getAbsolutePath(),
+            YouTrackDBConfig.defaultConfig())) {
+      youTrackDB.createIfNotExists(StorageEncryptionTestIT.class.getSimpleName(),
+          DatabaseType.DISK,
+          youTrackDBConfig,
+          "admin", "admin", "admin");
     }
-    try (final YouTrackDB youTrackDB =
-        new YouTrackDBImpl(
-            DbTestBase.embeddedDBUrl(getClass()), YouTrackDBConfig.defaultConfig())) {
-      final var youTrackDBConfig =
-          (YouTrackDBConfigImpl) YouTrackDBConfig.builder()
-              .addGlobalConfigurationParameter(GlobalConfiguration.STORAGE_ENCRYPTION_KEY,
-                  "T1JJRU5UREJfSVNfQ09PTA==")
-              .build();
-      try (var db =
-          (DatabaseSessionInternal) youTrackDB.open("encryption", "admin", "admin",
+    try (final var youTrackDB =
+        YourTracks.embedded(
+            dbDirectoryFile.getAbsolutePath(), YouTrackDBConfig.defaultConfig())) {
+      try (var session =
+          youTrackDB.open(StorageEncryptionTestIT.class.getSimpleName(), "admin", "admin",
               youTrackDBConfig)) {
-        final Schema schema = db.getMetadata().getSchema();
+        final var schema = session.getSchema();
         final var cls = schema.createClass("EncryptedData");
 
-        final var document = ((EntityImpl) db.newEntity(cls));
+        var tx = session.begin();
+        final var document = tx.newEntity(cls);
         document.setProperty("id", 10);
         document.setProperty(
             "value",
@@ -196,10 +198,17 @@ public class StorageEncryptionTestIT {
                 + " incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis"
                 + " nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
                 + " ");
+        tx.commit();
+      }
 
-        try (var resultSet = db.query("select from EncryptedData where id = ?", 10)) {
+      try (var session =
+          youTrackDB.open(StorageEncryptionTestIT.class.getSimpleName(), "admin", "admin",
+              youTrackDBConfig)) {
+        var tx = session.begin();
+        try (var resultSet = tx.query("select from EncryptedData where id = ?", 10)) {
           assertTrue(resultSet.hasNext());
         }
+        tx.commit();
       }
     }
   }
