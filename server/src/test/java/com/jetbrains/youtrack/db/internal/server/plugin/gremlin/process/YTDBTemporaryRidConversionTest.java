257c3da3e3af99cc49b45b41e40e9adf7f7bacc9
andrii0lomakin <andrii0lomakin@gmail.com>
2025-08-21 11:39:32 +0200

    More sophisticated Tx test was added.

--- server/src/test/java/com/jetbrains/youtrack/db/internal/server/plugin/gremlin/process/YTDBTemporaryRidConversionTest.java
+++ server/src/test/java/com/jetbrains/youtrack/db/internal/server/plugin/gremlin/process/YTDBTemporaryRidConversionTest.java
@@ -12,7 +12,6 @@ import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.apache.tinkerpop.gremlin.util.iterator.IteratorUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,33 +20,81 @@ import org.junit.runner.RunWith;
 @RunWith(GremlinProcessRunner.class)
 public abstract class YTDBTemporaryRidConversionTest extends AbstractGremlinTest {
 
-  public abstract Traversal<?, ?> get_g_addV_repeat128();
+  public abstract Traversal<?, ?> get_g_addV_repeat128_with_fail_in_first_step(int firstSteps);
+
+  public abstract Traversal<?, ?> get_g_addV_repeat128_with_fail_in_second_step(int firstSteps);
+
+  public abstract Traversal<?, ?> get_g_addV_repeat128_in_two_steps(int firstSteps);
 
   @Test
   @LoadGraphWith(MODERN)
   @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_ADD_VERTICES)
-  @FeatureRequirement(featureClass = Graph.Features.VertexFeatures.class, feature = Graph.Features.VertexFeatures.FEATURE_ADD_PROPERTY)
-  public void g_V_addV_repeat128() {
-    var initialCount = IteratorUtils.count(g.V());
-    var traversal = get_g_addV_repeat128();
-    var values = traversal.toList();
+  public void g_V_addV_repeat128_in_tx() {
+    for (var i = 1; i < 128; i++) {
+      long initialCount = g.V().count().next();
+      var traversal = get_g_addV_repeat128_in_two_steps(i);
+      var values = traversal.toList();
 
-    for (var v : values) {
-      Assert.assertTrue(v instanceof Vertex);
-      var vertex = (Vertex) v;
-      var rid = (RecordId) vertex.id();
-      Assert.assertTrue(rid.isPersistent());
+      Assert.assertEquals(128, values.size());
+      for (var v : values) {
+        Assert.assertTrue(v instanceof Vertex);
+        var vertex = (Vertex) v;
+        var rid = (RecordId) vertex.id();
+        Assert.assertTrue(rid.isPersistent());
+      }
+      assertEquals(initialCount + 128, g.V().count().next().longValue());
+      initialCount += 128;
+
+      traversal = get_g_addV_repeat128_with_fail_in_first_step(i);
+      try {
+        traversal.iterate();
+        Assert.fail("Should fail");
+      } catch (Exception e) {
+        Assert.assertTrue(e.getMessage().contains("exception during vertex creation"));
+      }
+
+      assertEquals(initialCount, g.V().count().next().longValue());
+
+      traversal = get_g_addV_repeat128_with_fail_in_second_step(i);
+      try {
+        traversal.iterate();
+        Assert.fail("Should fail");
+      } catch (Exception e) {
+        Assert.assertTrue(e.getMessage().contains("exception during vertex creation"));
+      }
+
+      assertEquals(initialCount, g.V().count().next().longValue());
     }
-
-    assertEquals(initialCount + 128, IteratorUtils.count(g.V()));
   }
 
   @SuppressWarnings("NewClassNamingConvention")
   public static class Traversals extends YTDBTemporaryRidConversionTest {
+    @Override
+    public Traversal<?, ?> get_g_addV_repeat128_with_fail_in_first_step(int firstSteps) {
+      //noinspection unchecked
+      return g.inject((Vertex) null).union(
+          __.repeat(__.addV("person")).times(firstSteps).fail("exception during vertex creation"),
+          __.repeat(__.addV("person")).times(128 - firstSteps)
+      );
+    }
 
     @Override
-    public Traversal<?, ?> get_g_addV_repeat128() {
-      return g.inject((Vertex) null).repeat(__.addV("person")).times(128).emit();
+    public Traversal<?, ?> get_g_addV_repeat128_with_fail_in_second_step(int firstSteps) {
+      //noinspection unchecked
+      return g.inject((Vertex) null).union(
+          __.repeat(__.addV("person")).times(firstSteps),
+          __.repeat(__.addV("person")).times(128 - firstSteps)
+              .fail("exception during vertex creation")
+      );
+    }
+
+    @Override
+    public Traversal<?, ?> get_g_addV_repeat128_in_two_steps(int firstSteps) {
+      //noinspection unchecked
+      return g.inject((Vertex) null).union(
+          __.repeat(__.addV("person")).times(firstSteps).emit(),
+          __.repeat(__.addV("person")).times(128 - firstSteps).emit()
+      );
     }
   }
 }
