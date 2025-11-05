945565da380852bbd540fdb8311fe84619ff98f5
andrii0lomakin <andrii0lomakin@gmail.com>
2025-11-05 16:19:50 +0100

    TinkerGraph serializer was added to the tests and TikerGraph was added as server dependency to ensure that `subgraph` step is working.

--- server/src/test/java/com/jetbrains/youtrackdb/internal/server/plugin/gremlin/YTDBGraphSONRemoteGraphProvider.java
+++ server/src/test/java/com/jetbrains/youtrackdb/internal/server/plugin/gremlin/YTDBGraphSONRemoteGraphProvider.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import org.apache.tinkerpop.gremlin.driver.AuthProperties;
 import org.apache.tinkerpop.gremlin.driver.AuthProperties.Property;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerIoRegistryV3;
 import org.apache.tinkerpop.gremlin.util.MessageSerializer;
 import org.apache.tinkerpop.gremlin.util.ser.AbstractMessageSerializer;
 import org.apache.tinkerpop.gremlin.util.ser.GraphSONMessageSerializerV3;
 
 public class YTDBGraphSONRemoteGraphProvider extends YTDBAbstractRemoteGraphProvider {
+
   public YTDBGraphSONRemoteGraphProvider() {
     super(createClusterBuilder(createSerializer()).authProperties(new AuthProperties().with(
         Property.USERNAME, "root").with(Property.PASSWORD, "root")).create());
@@ -20,8 +22,11 @@ public class YTDBGraphSONRemoteGraphProvider extends YTDBAbstractRemoteGraphProv
 
     var config = new HashMap<String, Object>();
     var ytdbIoRegistry = YTDBIoRegistry.class.getName();
+    var tinkerGraphIoRegistry = TinkerIoRegistryV3.class.getName();
+
     var registries = new ArrayList<String>();
     registries.add(ytdbIoRegistry);
+    registries.add(tinkerGraphIoRegistry);
 
     config.put(AbstractMessageSerializer.TOKEN_IO_REGISTRIES, registries);
     graphSonSerializer.configure(config, null);
