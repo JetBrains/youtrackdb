b7778da9bb15fb7b01248f14383974056e11b6bf
artem.loginov <artem.loginov@audienzz.ch>
2026-01-27 11:01:45 +0100

    #YTDB-165
    Fix test with random values.

--- core/src/test/java/com/jetbrains/youtrackdb/internal/common/concur/collection/CASObjectArrayMTTest.java
+++ core/src/test/java/com/jetbrains/youtrackdb/internal/common/concur/collection/CASObjectArrayMTTest.java
@@ -1,5 +1,6 @@
 package com.jetbrains.youtrackdb.internal.common.concur.collection;
 
+import java.util.concurrent.ThreadLocalRandom;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.pastalab.fray.junit.junit5.FrayTestExtension;
 import org.pastalab.fray.junit.junit5.annotations.ConcurrencyTest;
@@ -8,16 +9,17 @@ import org.pastalab.fray.junit.junit5.annotations.ConcurrencyTest;
 public class CASObjectArrayMTTest {
 
   public void addShouldReturnCorrectIndex() throws InterruptedException {
-    var account = new CASObjectArray<Integer>();
+    ThreadLocalRandom.current();
+    var array = new CASObjectArray<Integer>();
     var t1 = new Thread(() -> {
-      Integer value = 500;
-      var idx = account.add(value);
-      assert (account.get(idx).equals(value));
+      Integer value = ThreadLocalRandom.current().nextInt();
+      var idx = array.add(value);
+      assert (array.get(idx).equals(value));
     });
     var t2 = new Thread(() -> {
-      Integer value = 500;
-      var idx = account.add(value);
-      assert (account.get(idx).equals(value));
+      Integer value = ThreadLocalRandom.current().nextInt();
+      var idx = array.add(value);
+      assert (array.get(idx).equals(value));
     });
     t1.start();
     t2.start();
