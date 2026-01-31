a1be20fbd46429498e9616678235dff33fa097ac
Lev Sivashov <lev.sivashov@jetbrains.com>
2026-01-31 13:58:53 +0100

    YTDB-154: YTDBQueryConfigParam.querySummary support

--- core/src/main/java/com/jetbrains/youtrackdb/api/gremlin/tokens/YTDBQueryConfigParam.java
+++ core/src/main/java/com/jetbrains/youtrackdb/api/gremlin/tokens/YTDBQueryConfigParam.java
@@ -1,8 +1,22 @@
 package com.jetbrains.youtrackdb.api.gremlin.tokens;
 
+import javax.annotation.Nullable;
+import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
+import org.apache.tinkerpop.gremlin.process.traversal.strategy.decoration.OptionsStrategy;
+
+/// YTDB-specific parameters that can be passed to
+/// [[com.jetbrains.youtrackdb.api.gremlin.YTDBGraphTraversalSource#with(YTDBQueryConfigParam,
+/// Object)]] and
+/// [[com.jetbrains.youtrackdb.api.gremlin.YTDBGraphTraversalSource#with(YTDBQueryConfigParam)]]
+/// methods to configure query behavior.
 public enum YTDBQueryConfigParam {
 
-  polymorphicQuery(Boolean.class);
+  /// Controls whether the query is polymorphic, i.e., subclasses can be queried by their parent
+  /// classes' names.
+  polymorphicQuery(Boolean.class),
+
+  /// Client-provided query summary for query monitoring purposes.
+  querySummary(String.class);
 
   private final Class<?> type;
 
@@ -12,5 +26,16 @@ public enum YTDBQueryConfigParam {
 
   public Class<?> type() {
     return type;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T> @Nullable T getValue(Traversal.Admin<?, ?> traversal) {
+    final var strategy = traversal.getStrategies().getStrategy(OptionsStrategy.class).orElse(null);
+    if (strategy == null) {
+      return null;
+    }
+    final var value = strategy.getOptions().get(this.name());
+
+    return (T) value;
   }
 }
