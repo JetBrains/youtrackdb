9b4229d4041064daa34cb36767eeeba87e045e51
sandrawar <sandra.a3@o2.pl>
2026-02-09 11:52:35 +0100

    YTDB-507:YTDB-507-cleanup

--- core/src/test/java/com/jetbrains/youtrackdb/internal/core/gql/parser/GqlStatementCacheTest.java
+++ core/src/test/java/com/jetbrains/youtrackdb/internal/core/gql/parser/GqlStatementCacheTest.java
@@ -8,24 +8,28 @@ public class GqlStatementCacheTest extends DbTestBase {
 
   @Test
   public void testLRUEvictionLogic() {
+    // Initialize cache with a size of 2 to easily test eviction
     var cache = new GqlStatementCache(2);
 
     cache.getCached("MATCH (a:Person)");
     cache.getCached("MATCH (b:Company)");
 
+    // Adding a third item should evict the oldest one (a:Person)
     cache.getCached("MATCH (c:Product)");
 
     Assert.assertTrue(cache.contains("MATCH (b:Company)"));
     Assert.assertTrue(cache.contains("MATCH (c:Product)"));
-    Assert.assertFalse("Pierwsze zapytanie powinno zostać usunięte", cache.contains("MATCH (a:Person)"));
+    Assert.assertFalse("The first query should have been evicted", cache.contains("MATCH (a:Person)"));
 
+    // Accessing (b:Company) to make it the most recently used
     cache.getCached("MATCH (b:Company)");
 
+    // Adding (d:Location) should now evict (c:Product)
     cache.getCached("MATCH (d:Location)");
 
     Assert.assertTrue(cache.contains("MATCH (b:Company)"));
     Assert.assertTrue(cache.contains("MATCH (d:Location)"));
-    Assert.assertFalse("Zapytanie (c:Product) powinno wypaść, bo (b:Company) zostało odświeżone",
+    Assert.assertFalse("Query (c:Product) should be evicted because (b:Company) was refreshed",
         cache.contains("MATCH (c:Product)"));
   }
 
@@ -33,15 +37,16 @@ public class GqlStatementCacheTest extends DbTestBase {
   public void testCacheIntegrationWithSession() {
     var query = "MATCH (n:User) WHERE n.id = 1";
 
-    // 1. Pobieramy przez statyczną metodę GqlStatementCache.get
-    // To wewnętrznie odwoła się do session.getSharedContext().getGqlStatementCache()
+    // 1. Retrieve using the static GqlStatementCache.get method
+    // This internally refers to session.getSharedContext().getGqlStatementCache()
     var first = GqlStatementCache.get(query, session);
 
-    // 2. Pobieramy drugi raz
+    // 2. Retrieve a second time
     var second = GqlStatementCache.get(query, session);
 
-    // 3. Sprawdzamy czy to dokładnie ta sama instancja (referencja)
+    // 3. Verify it is exactly the same instance (reference)
     Assert.assertNotNull(first);
-    Assert.assertSame("GQL Statement powinien być pobrany z cache'u SharedContext", first, second);
+    Assert.assertSame("GQL Statement should be retrieved from the SharedContext cache", first,
+        second);
   }
 }
