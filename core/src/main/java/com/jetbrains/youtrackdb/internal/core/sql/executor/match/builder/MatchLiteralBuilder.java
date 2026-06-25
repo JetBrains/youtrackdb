8ba847ed9aea0975cf794b49cc66c254dec4c2ac
sandrawar <sandra.a3@o2.pl>
2026-06-25 22:52:28 +0200

    YTDB-558: cleanup

--- core/src/main/java/com/jetbrains/youtrackdb/internal/core/sql/executor/match/builder/MatchLiteralBuilder.java
+++ core/src/main/java/com/jetbrains/youtrackdb/internal/core/sql/executor/match/builder/MatchLiteralBuilder.java
@@ -48,37 +48,37 @@ public final class MatchLiteralBuilder {
   public static SQLExpression toLiteral(Object value) {
     Objects.requireNonNull(value, "value must not be null");
     var expr = new SQLExpression(-1);
-      switch (value) {
-          case String s -> {
-              expr.setMathExpression(new SQLBaseExpression(s));
-              return expr;
-          }
-          case RecordIdInternal rid -> {
-              var sqlRid = new SQLRid(-1);
-              var collection = new SQLInteger(-1);
-              collection.setValue(rid.getCollectionId());
-              var position = new SQLInteger(-1);
-              position.setValue(rid.getCollectionPosition());
-              sqlRid.setCollection(collection);
-              sqlRid.setPosition(position);
-              sqlRid.setLegacy(true);
-              expr.setRid(sqlRid);
-              return expr;
-          }
-          case Number n -> {
-              var integer = new SQLInteger(-1);
-              integer.setValue(n);
-              expr.setMathExpression(new SQLBaseExpression(integer));
-              return expr;
-          }
-          case Boolean b -> {
-              expr.setBooleanValue(b);
-              return expr;
-          }
-          default -> {
-          }
+    switch (value) {
+      case String s -> {
+        expr.setMathExpression(new SQLBaseExpression(s));
+        return expr;
       }
-      if (value instanceof Date
+      case RecordIdInternal rid -> {
+        var sqlRid = new SQLRid(-1);
+        var collection = new SQLInteger(-1);
+        collection.setValue(rid.getCollectionId());
+        var position = new SQLInteger(-1);
+        position.setValue(rid.getCollectionPosition());
+        sqlRid.setCollection(collection);
+        sqlRid.setPosition(position);
+        sqlRid.setLegacy(true);
+        expr.setRid(sqlRid);
+        return expr;
+      }
+      case Number n -> {
+        var integer = new SQLInteger(-1);
+        integer.setValue(n);
+        expr.setMathExpression(new SQLBaseExpression(integer));
+        return expr;
+      }
+      case Boolean b -> {
+        expr.setBooleanValue(b);
+        return expr;
+      }
+      default -> {
+      }
+    }
+    if (value instanceof Date
         || value instanceof List<?>
         || value instanceof Set<?>
         || value instanceof Map<?, ?>
